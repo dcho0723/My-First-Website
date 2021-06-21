db91c633eb705d8bd1c6ec2ff18e640529c5d8ff
Dennis Cho <dcho0723@yahoo.com>
2021-06-21 00:28:27 -0400

    fixed dog/patrick

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ function addingEventListener() {
     })
 }
 addingEventListener();
+
+
+function changeImage() {
+
+    if (document.getElementById("imgClickAndChange").src == "https://static.wikia.nocookie.net/spongebob/images/c/c7/1f9.png/revision/latest?cb=20181110203331") 
+        {
+            document.getElementById("imgClickAndChange").src = "https://i.kym-cdn.com/photos/images/original/000/217/040/48ACD.png";
+        }
+    else 
+        {
+            document.getElementById("imgClickAndChange").src = "https://static.wikia.nocookie.net/spongebob/images/c/c7/1f9.png/revision/latest?cb=20181110203331";
+        }
+    }
+changeImage();
