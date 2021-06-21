c5963d091f7fcacdd71713873d20dc938e7b9f95
Dennis Cho <dcho0723@yahoo.com>
2021-06-20 21:11:29 -0400

    Finished Assignment

---
+++ script.js
@@ -0,0 +1,8 @@
+function addingEventListener() {
+    const thanks = document.getElementById("thanks");
+
+    thanks.addEventListener('click', function() {
+        alert('I Hope You Have A Great Day');
+    })
+}
+addingEventListener();
