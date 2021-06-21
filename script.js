be09326f315e903b764d19141db6a370c320b612
Dennis Cho <dcho0723@yahoo.com>
2021-06-21 00:34:52 -0400

    patrick/dog fix

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ addingEventListener();
 
 function changeImage() {
 
-    if (document.getElementById("imgClickAndChange").src == "https://static.wikia.nocookie.net/spongebob/images/c/c7/1f9.png/revision/latest?cb=20181110203331") 
+    if (document.getElementById("imgClickAndChange").src == "https://www.memesmonkey.com/images/memesmonkey/1d/1d3f97f6e5ca061b97074bb68063a76e.jpeg") 
         {
             document.getElementById("imgClickAndChange").src = "https://i.kym-cdn.com/photos/images/original/000/217/040/48ACD.png";
         }
     else 
         {
-            document.getElementById("imgClickAndChange").src = "https://static.wikia.nocookie.net/spongebob/images/c/c7/1f9.png/revision/latest?cb=20181110203331";
+            document.getElementById("imgClickAndChange").src = "https://www.memesmonkey.com/images/memesmonkey/1d/1d3f97f6e5ca061b97074bb68063a76e.jpeg";
         }
     }
 changeImage();
